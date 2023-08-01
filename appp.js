612964a8cf4a1628b341cac02f88b545eb3a9ded
Mohit-3007 <mohitkhurana30@gmail.com>
2023-08-01 18:10:28 +0530

    bug fixes

--- appp.js
+++ appp.js
@@ -1,30 +1,40 @@
-let   inputArea = document.getElementById("inputArea");
-const operation = document.getElementById("category-list");
-const searchBtn = document.getElementById("searchBtn");
-const savedSol = document.getElementById("savedSol");
-const main = document.querySelector(".main");
-const solContainer = document.querySelector(".solContainer");
-const problemBtn = document.getElementById("problemBtn");
-const cardContainer = document.createElement("div");
-const form = document.getElementById("myForm");
+let inputArea = document.getElementById("inputArea");
 
+const operation = document.getElementById("category-list");
+
+const searchBtn = document.getElementById("searchBtn");
+
+const savedSol = document.getElementById("savedSol");
+
+const main = document.querySelector(".main");
+
+const solContainer = document.querySelector(".solContainer");
+
+const problemBtn = document.getElementById("problemBtn");
+
+const cardContainer = document.createElement("div");
+
+const form = document.getElementById("myForm");
 
 problemBtn.addEventListener("click", () => {
   solContainer.classList.add("hidden");
+
   main.classList.remove("hidden");
 });
 
 inputArea.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     console.log(e);
+
     e.preventDefault();
+
     encodeInput();
   }
 });
 
 searchBtn.addEventListener("click", (e) => {
-  console.log(e);
-  e.preventDefault()
+  e.preventDefault();
+
   encodeInput();
 });
 
@@ -32,40 +42,54 @@ function encodeInput() {
   if (inputArea.value === "") {
     console.log("empty string");
   } else {
-  
     let expression = encodeURIComponent(inputArea.value);
+
     console.log(expression);
+
     getData(expression, operation.value);
-
-
   }
 }
 
 async function getData(exp, op) {
   const resp = await fetch(`https://newton.vercel.app/api/v2/${op}/${exp}`);
+
   const data = await resp.json();
+
   console.log(data);
-  //const main = document.querySelector(".main");
- // main.innerHTML 
+
+  const main = document.querySelector(".main");
+
   const htmlCOntent = `
-    <div class="main-three">
-      <div class="problem">
-        <p><span>${op}</span> : ${data.expression}</p>
-      </div>  
-      <div class="solution">
-        <p>${data.result}</p>
-      </div>
-      <div class="delete-btn">
-        <button id="deleteBtn"><i class="fa-solid fa-trash-can fa-2xl" style="color: #080808;"></i></button>
-      </div> 
-  `;
-  
-  //console.log(main_three);
-  // main_three.innerHTML
+
+<div class="main-three">
+
+<div class="problem">
+
+<p><span>${op}</span> : ${data.expression}</p>
+
+</div>
+
+<div class="solution">
+
+<p>${data.result}</p>
+
+</div>
+
+<div class="delete-btn">
+
+<button id="deleteBtn"><i class="fa-solid fa-trash-can fa-2xl" style="color: #080808;"></i></button>
+
+</div>
+
+`;
+
   main.innerHTML += htmlCOntent;
+
   saveData(data);
+
   let deleteBtn = document.getElementById("deleteBtn");
-  DeleteResultWindow();
+
+  callDeleteLocal();
 }
 
 function saveData(data) {
@@ -73,23 +97,30 @@ function saveData(data) {
 
   if (!localData) {
     console.log(data);
+
     let arr = [];
+
     arr.push(data);
+
     window.localStorage.setItem("localData", JSON.stringify(arr));
   } else {
     let array = JSON.parse(localStorage.getItem("localData"));
+
     array.push(data);
+
     localStorage.setItem("localData", JSON.stringify(array));
   }
 }
 
-function DeleteResultWindow() {
+function callDeleteLocal() {
   deleteBtn.addEventListener("click", function () {
-    console.log("delete result window");
-    const main_three = document.querySelector(".main-three");
-    if (main_three) {
-      main.removeChild(main_three);
-    } 
+    console.log("deleteLocal function is called");
+
+    let array = JSON.parse(localStorage.getItem("localData"));
+
+    array.splice(-1, 1);
+
+    window.localStorage.setItem("localData", JSON.stringify(array));
   });
 }
 
@@ -109,8 +140,11 @@ const deleteCard = document.getElementById("deleteCard");
 
 function cardDelete(event) {
   console.log(event);
+
   let array = JSON.parse(localStorage.getItem("localData"));
+
   array.splice(event, 1);
+
   window.localStorage.setItem("localData", JSON.stringify(array));
 
   savedSolution();
@@ -118,26 +152,41 @@ function cardDelete(event) {
 
 function savedSolution() {
   console.log("button clicked");
+
   main.classList.add("hidden");
+
   solContainer.classList.remove("hidden");
+
   cardContainer.innerHTML = "";
+
   cardContainer.classList.add("card-container");
 
   const storaaa = JSON.parse(window.localStorage.getItem("localData"));
+
   console.log(storaaa);
+
   if (storaaa == null || storaaa == "undefined") {
     console.log("No Data To Render");
+
     return;
   } else {
     storaaa.forEach((e, idx) => {
       const cardHtml = `
-        <div class="card">
-          <button id="deleteCard" value=${idx}>X</button>
-          <p class="para p1">${e.operation}</p>
-          <textarea id="textArea" disabled>${e.expression}</textarea>
-          <p class="para p2">${e.result}</p>
-        </div>
-      `;
+
+<div class="card">
+
+<button id="deleteCard" value=${idx}>X</button>
+
+<p class="para p1">${e.operation}</p>
+
+<textarea id="textArea" disabled>${e.expression}</textarea>
+
+<p class="para p2">${e.result}</p>
+
+</div>
+
+`;
+
       cardContainer.innerHTML += cardHtml;
     });
 
